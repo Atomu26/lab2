ad1755abc9d14236d05b46916bfb3730754e2914
Pem Tsering Gurung <121584676+pemtseringgurung@users.noreply.github.com>
2024-09-02 12:47:53 -0400

    Pem Update fibonacciTesting.py

--- fibonacciTesting.py
+++ fibonacciTesting.py
@@ -3,6 +3,10 @@ import unittest
 class Parent:
     def division(self, x):
         return x / 2 
+    def addition(self, y):
+        return y + 2
+    def subtraction(self, z):
+        return z - 2 
 
 class Child(Parent):
     pass
@@ -15,7 +19,13 @@ class TestMultiply(unittest.TestCase):
 
     def test_multiply_again(self):
         child_instance_second = Child()
-        self.assertEqual(child_instance_second.multiply(16), 8.0)  
+        self.assertEqual(child_instance_second.multiply(16), 8.0) 
+    def test_addition(self):
+        child_instance_third = Child()
+        self.assertEqual(child_instance_third.addition(5), 7)
+    def test_subtraction(self):
+        child_instance_third = Child()
+        self.assertEqual(child_instance_third.subtraction(16), 14)
 
 
 
